590f2b448d9812a6ac2ac52d02651d74863d9cc5
8483developer <140682311+8483developer@users.noreply.github.com>
2025-01-26 18:55:52 +0530

    Update Homepage.jsx

--- src/components/pages/Homepage.jsx
+++ src/components/pages/Homepage.jsx
@@ -18,6 +18,7 @@ const Homepage = () => {
               src="/assets/images/crousal/DALL·E 2024-12-13 08.29.11 - A modern homepage design for a furniture website named 'CraftNest.' The homepage features a visually stunning banner carousel with high-quality images.webp"
               className="block w-full h-full object-cover"
               alt="Banner 1"
+              loading="eager"
             />
           </div>
           {/* Item 2 */}
